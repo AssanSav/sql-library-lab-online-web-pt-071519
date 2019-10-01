85bec7eb12fb52328a72a48fe6266e65a7bc194b
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 17:49:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.years"
+  "SELECT books.title, books.years FROM books 
+  JOIN series ON series.id = books.serie_id
+  ORDER BY books.years"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
