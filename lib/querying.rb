4b2d5a33934b28f008c474a49182c86c55c0506f
assane savadogo <assane.savadogo81@gmail.com>
2019-10-01 18:17:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
-  JOIN characters ON series.id = characters.serie_id
+  JOIN characters ON series.id = characters.author_id
   WHERE characters.species = 'human'
   GROUP BY series.title
   ORDER BY COUNT(characters.species) DESC LIMIT 1"
